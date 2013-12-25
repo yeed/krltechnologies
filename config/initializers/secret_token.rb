d0d07e88e491a053bbb8e5e3959ed83af5f038d0
Yeed <yoktars@gmail.com>
2013-12-25 14:17:51 +0200

    v1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krltechnologies::Application.config.secret_token = 'c03fe8909905927ccf82ebd3eb7b733f14ec8bfaa188cded850522cb8a87a7ff0762c41ee1ef27235f4a3492e9b7e87d471912c219541a46af0c670a0c2f2095'
